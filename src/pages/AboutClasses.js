bbed775672f64062bca6b0f65e987dd84040c7c6
lazar <lazarnikolic120@gmail.com>
2024-12-30 13:16:48 +0100

    sdtbg12-2

--- src/pages/AboutClasses.js
+++ src/pages/AboutClasses.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Navlink, Link } from "react-router-dom";
 
 export default function AboutClasses() {
-  return (
-    <>
-      <></>
-    </>
-  );
+	return (
+		<>
+			<></>
+		</>
+	);
 }
